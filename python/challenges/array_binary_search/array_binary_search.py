e0db6ab0b1e426a0ad2ca87820941243bb6b1b2a
YahyaOmari <yahya.omari@outlook.com>
2021-05-05 00:26:03 +0300

    tried new way

--- python/challenges/array_binary_search/array_binary_search.py
+++ python/challenges/array_binary_search/array_binary_search.py
@@ -1,12 +1,24 @@
-def BinarySearch(list,key):
-    x = key
-    if key in list:
-        print(2)
-        return 2
-    else:
-        print(-1)
-        return -1
-    # return key
+# First Way
+
+
+# def BinarySearch(list,key):
+#     x = key
+#     if key in list:
+#         print(2)
+#         return 2
+#     else:
+#         print(-1)
+#         return -1
+#     # return key
+
+# Second Way
+def BinarySearch(list, key):
+    for i in range(len(list)):
+        if list[i] == key:
+            print(2)
+            return 2
+    print(-1)
+    return -1    
 
 BinarySearch([4,8,15,16,23,42], 42)
 
